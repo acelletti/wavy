b9c9794a99d86ef52008d06a6f270172e873899a
Andrea Celletti <celletti.andrea87@gmail.com>
2018-03-24 18:42:09 +0000

    Added integration test for info function. Also added checks for Tags on audio files.

--- test/integration/test_info.py
+++ test/integration/test_info.py
@@ -6,16 +6,15 @@ from test.utils import *
 @pytest.mark.parametrize('file', get_audio_files(), ids=lambda x: str(x))
 def test_read(file):
     """
-    Test read function with real audio files.
+    Test info function with real audio files.
     """
-    # read file
-    result = read(file.file_path)
+    # read file information
+    result = info(file.file_path)
 
     # check that file info matches expected
     assert result.sample_width == file.sample_width
     assert result.framerate == file.framerate
     assert result.n_channels == file.n_channels
     assert result.n_frames == file.n_frames
-    assert result.data.dtype == file.dtype
     assert result.tags == file.tags
 
--- test/utils/get_audio_files.py
+++ test/utils/get_audio_files.py
@@ -1,6 +1,7 @@
 import numpy
 import os
 import re
+from wavy import Tags
 
 # audio files for testing
 AUDIO_FILES_FOLDER = 'test/integration/data'
@@ -8,11 +9,23 @@ AUDIO_FILES_FOLDER = 'test/integration/data'
 # extract info from file name
 AUDIO_FILES_REGEX = r"(\d+)bit_(\d+)Hz_(\d+)ch_(\d+)fr_([a-z]+)"
 
+# identify files by number of frames
+TAGS = {
+    # file used for testing in cpython builtin.wave module
+    3307: Tags(name='Pluck', subject='', artist='Serhiy Storchaka',
+               comment='Audacity Pluck + Wahwah', keywords='',
+               software='', engineer='', technician='',
+               creation_date='2013', genre='', copyright=''),
+    # sample from http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/Samples.html M1F1 file
+    23493: None
+}
+
 
 class TestAudioFile(object):
     """
     Class to store audio file info.
     """
+
     def __init__(self, sample_width, framerate,
                  n_channels, n_frames, dtype,
                  file_path):
@@ -24,12 +37,18 @@ class TestAudioFile(object):
         self.n_frames = int(n_frames)
         self.file_path = file_path
 
+        self.tags = TAGS[self.n_frames]
+
         # work out dtype for data
         if dtype == 'pcm':
             if self.sample_width == 8:
                 self.dtype = 'u1'
             elif self.sample_width == 24:
                 self.dtype = 'i4'
+                # when converting to this one to extensible sox removed
+                # the tag
+                if '_ext' in self.file_path:
+                    self.tags = None
             else:
                 self.dtype = 'i{}'.format(self.sample_width // 8)
         else:
@@ -47,7 +66,7 @@ def get_all_files(dir_name):
     Get all files in the folder in alphabetic order.
     """
     return list(sorted(filter(os.path.isfile,
-        [os.path.join(dir_name, file) for file in os.listdir(dir_name)])))
+                              [os.path.join(dir_name, file) for file in os.listdir(dir_name)])))
 
 
 def get_audio_files():
